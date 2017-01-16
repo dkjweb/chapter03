c274778299c3c556ae7de468b8de90dc5a6f3d8b
T3AD20 <T3AD20@T3AD20C>
2017-01-16 09:02:43 +0900

    equals & hashCode 오버라이딩

--- src/chapter03/Rect.java
+++ src/chapter03/Rect.java
@@ -27,6 +27,31 @@ public class Rect {
 		return "Rect [width=" + width + ", height=" + height + "]";
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (height * width);
+		
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Rect other = (Rect) obj;
+		if (height != other.height)
+			return false;
+		if (width != other.width)
+			return false;
+		return true;
+	}
+
 
 
 	
